61adb93dcabede177eb1ebcf45ee1f2daab5d892
Amom <amom_augusto@hotmail.com>
2021-06-10 23:17:00 -0300

    implementado ação do food

--- js/script.js
+++ js/script.js
@@ -47,6 +47,8 @@ function startGame(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    
+
     createBG();
     createSnake();
     drawFood();
@@ -59,7 +61,12 @@ function startGame(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    }
+    else{ food.x = Math.floor(Math.random() * 15 + 1) * box;
+          food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,

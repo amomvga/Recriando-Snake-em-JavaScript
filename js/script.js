a5dc9e28d1e8cba67e43413af821dffa29649233
Amom <amom_augusto@hotmail.com>
2021-06-10 22:12:41 -0300

    criação do movimento

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
-let snake = [];
 
+let snake = [];
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+
+let direction = "right";
 
 function createBG() {
     context.fillStyle = "lightgreen";
@@ -20,5 +22,26 @@ function createSnake() {
     }
 }
 
-createBG();
-createSnake();
+function startGame(){
+    createBG();
+    createSnake()
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
